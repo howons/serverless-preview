b95615b1d20922c4dec8f266e721bcb5e6500156
howons <shinhw371@gmail.com>
2024-01-06 01:45:28 +0900

    chore: route 재 정의

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-import v1 from "./v1";
+import home from "./home/route";
 
 const router = Router();
 
-router.use("/v1", v1);
+router.use("/home", home);
 
 export default router;
--- server/routes/v1/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import pages from "./pages.route";
-
-const router = Router();
-
-router.use("/pages", pages);
-
-export default router;
