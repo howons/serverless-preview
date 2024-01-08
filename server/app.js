f24560b7809b07c09c761416b91ad79896e85501
howons <shinhw371@gmail.com>
2024-01-08 12:11:39 +0900

    feat: webpack static file 접근 추가

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import routes from './routes';
 
@@ -6,20 +7,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/src', express.static('../src'));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.use('/', routes);
 
-app.use(
-  (req, res, next) => {
-    res.status(404).send();
-  },
-);
+app.use((req, res, next) => {
+  res.status(404).send();
+});
 
-app.use(
-  (err, req, res, next) => {
-    res.status(err.status || 500).send();
-  },
-);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send();
+});
 
 export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const serverlessWebpack = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   // entry를 따로 설정하지 않아도 됨
@@ -10,8 +11,13 @@ module.exports = {
   // webpack의 critical warning 메시지를 피하기 위한 용도
   externals: [nodeExternals()],
   resolve: {
-    modules: [path.resolve('./server'), 'node_modules'] // server 디렉토리 내부에서 absolute import를 사용하기 위한 용도
+    modules: [path.resolve('./server'), 'node_modules'], // server 디렉토리 내부에서 absolute import를 사용하기 위한 용도
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: './server/src', to: 'server/src' }],
+    }),
+  ],
   module: {
     rules: [
       {
@@ -19,15 +25,15 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader'
-          }
-        ]
-      }
-    ]
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
-    filename: '[name].js'
-  }
+    filename: '[name].js',
+  },
 };
