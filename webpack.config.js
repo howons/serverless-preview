30320848e1512089436339613b163f2303d43280
howons <shinhw371@gmail.com>
2024-01-10 01:03:33 +0900

    chore: 웹팩 분리

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,9 @@ const serverlessWebpack = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+var configSls = {
   // entry를 따로 설정하지 않아도 됨
-  entry: {
-    ...serverlessWebpack.lib.entries,
-    'server/src/main': './server/src/main.js',
-  },
+  entry: serverlessWebpack.lib.entries,
   target: 'node',
   mode: serverlessWebpack.lib.webpack.isLocal ? 'development' : 'production',
   // webpack의 critical warning 메시지를 피하기 위한 용도
@@ -16,9 +13,36 @@ module.exports = {
   resolve: {
     modules: [path.resolve('./server'), 'node_modules'], // server 디렉토리 내부에서 absolute import를 사용하기 위한 용도
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack/service'),
+    filename: '[name].js',
+  },
+};
+
+var configSrc = {
+  entry: { 'server/src/main': './server/src/main.js' },
+  mode: serverlessWebpack.lib.webpack.isLocal ? 'development' : 'production',
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: './server/src/*.css', to: '.' }],
+      patterns: [{ from: './server/src/*.css', to: './' }],
     }),
   ],
   module: {
@@ -38,8 +62,9 @@ module.exports = {
     ],
   },
   output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
+    path: path.join(__dirname, '.webpack/service'),
     filename: '[name].js',
   },
 };
+
+module.exports = [configSls, configSrc];
