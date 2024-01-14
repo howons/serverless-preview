249e6606cf93b82942ecad93ca1ce0f8589c599c
howons <shinhw371@gmail.com>
2024-01-14 19:06:16 +0900

    rename: models 병합

--- server/components/sidebar/index.js
+++ server/components/sidebar/index.js
@@ -1,4 +1,8 @@
-import { sidebarItems } from '../../models/sideItems';
+const sidebarItems = [
+  { name: '인트로', icon: 'H' },
+  { name: '프로필', icon: 'P' },
+  { name: '프로젝트 목록', icon: 'L' },
+];
 
 export const sidebar = `
   <aside class="sidebar">
--- server/models/sideItems.js
+++
@@ -1,5 +0,0 @@
-export const sidebarItems = [
-  { name: '인트로', icon: 'H' },
-  { name: '프로필', icon: 'P' },
-  { name: '프로젝트 목록', icon: 'L' },
-];
