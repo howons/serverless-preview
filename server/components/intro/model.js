e0fcd5b14315357bec42923970fd8e7ccd543f0d
howons <shinhw371@gmail.com>
2024-01-27 01:19:07 +0900

    feat: 인트로 페이지 내용

--- server/components/intro/model.js
+++ server/components/intro/model.js
@@ -3,10 +3,10 @@ export const intro = {
   style: `<link id="style-intro" rel="stylesheet" href="./src/intro.css" />`,
   main: `
     <div id="main" class="main intro">
-      <h1 class="intro__title">프론트엔드 개발자 신호원입니다</h1>
+      <h1 class="intro__title">기초를 다지는 프론트엔드 개발자, 신호원입니다</h1>
       <section class="intro__profile-summary">
         <p class="intro__profile-item">신호원</p>
-        <p class="intro__profile-item">010-6537-7020</p>
+        <p class="intro__profile-item">010-6535-7020</p>
         <p class="intro__profile-item">shinhw371@gmail.com</p>
       </section>
     </div>
