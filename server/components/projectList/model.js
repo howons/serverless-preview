3d6b2caa1db2de0f318e5eff12b5d4231f00efa6
howons <shinhw371@gmail.com>
2024-01-31 23:01:57 +0900

    feat: 모델 추가

--- server/components/projectList/model.js
+++ server/components/projectList/model.js
@@ -2,7 +2,12 @@ export const projectList = {
   id: '/project-list',
   content: `
     <div id="project-list" class="project-list">
-      
+      <section class="project-list__album">
+        사진들
+      </section>
+      <section class="project-list__description">
+        프로젝트 설명
+      </section>
     </div>
   `,
 };
