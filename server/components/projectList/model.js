45b97ebea6027d45b92c322cf28f658a29913d74
howons <shinhw371@gmail.com>
2024-02-01 20:23:33 +0900

    feat: 이미지 추가

--- server/components/projectList/model.js
+++ server/components/projectList/model.js
@@ -1,3 +1,23 @@
+import { IMAGE_URL } from '../../utils/images';
+
+const projects = [
+  {
+    id: 'portfolio',
+    url: null,
+    description:
+      'Vanilla JS와 Serverless로 만든 SSR 방식의 포트폴리오 사이트입니다.',
+    skills: [
+      'JavaScript',
+      'Serverless Framework',
+      'Express',
+      'Webpack',
+      'Sass',
+      'Aws Lambda',
+      'Aws S3',
+    ],
+  },
+];
+
 export const projectList = {
   id: '/project-list',
   content: `
--- server/utils/images.js
+++ server/utils/images.js
@@ -1,3 +1,22 @@
 export const IMAGE_URL = {
   PROFILE: 'https://d2pu0ruyxf50in.cloudfront.net/20231223_162052_cut.jpg',
+  HERO: 'https://d2pu0ruyxf50in.cloudfront.net/히어로.png',
+  MUSSEUK: 'https://d2pu0ruyxf50in.cloudfront.net/머쓱레터_og.png',
+  VELOG: 'https://d2pu0ruyxf50in.cloudfront.net/velog.png',
+};
+
+export const ICON_URL = {
+  JAVASCRIPT:
+    'https://img.shields.io/badge/JavaScript-%23F7DF1E?style=flat-square&logo=javascript&logoColor=white',
+  SASS: 'https://img.shields.io/badge/Sass-%23CC6699?style=flat-square&logo=sass&logoColor=white',
+  SERVERLESS:
+    'https://img.shields.io/badge/Serverless-%23FD5750?style=flat-square&logo=serverless&logoColor=white',
+  WEBPACK:
+    'https://img.shields.io/badge/Webpack-%238DD6F9?style=flat-square&logo=webpack&logoColor=white',
+  EXPRESS:
+    'https://img.shields.io/badge/Express-%23000000?style=flat-square&logo=express&logoColor=white',
+  AWS_S3:
+    'https://img.shields.io/badge/Aws%20S3-%23569A31?style=flat-square&logo=amazons3&logoColor=white',
+  AWS_LAMBDA:
+    'https://img.shields.io/badge/Aws%20Lambda-%23FF9900?style=flat-square&logo=awslambda&logoColor=white',
 };
