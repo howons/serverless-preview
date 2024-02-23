3b8ea09921b928195d799fe86dc2eca8f69f9a1d
howons <shinhw371@gmail.com>
2024-02-23 18:22:52 +0900

    chore: 스크립트 추가

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,15 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   // entry를 따로 설정하지 않아도 됨
-  entry: {
-    ...serverlessWebpack.lib.entries,
-  },
+  entry: serverlessWebpack.lib.entries,
   output: {
     libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack/service'),
+    path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
   },
   target: 'node',
   mode: serverlessWebpack.lib.webpack.isLocal ? 'development' : 'production',
-  // webpack의 critical warning 메시지를 피하기 위한 용도
+  // 의존성 파일을 번들링에 포함하지 않도록 함
   externals: [nodeExternals()],
   resolve: {
     modules: [path.resolve('./server'), 'node_modules'], // server 디렉토리 내부에서 absolute import를 사용하기 위한 용도
