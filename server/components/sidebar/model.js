b680e4dde9ef04056adabba074a3b893bf0037f0
howons <shinhw371@gmail.com>
2024-03-03 19:26:11 +0900

    style: 사이드바 아이콘 변경

--- server/components/sidebar/model.js
+++ server/components/sidebar/model.js
@@ -1,31 +1,38 @@
+import { SVG } from '../../utils/images';
+
 export const sidebarItems = [
-  { id: 'side-intro', name: '인트로', icon: 'H', path: '/intro' },
-  { id: 'side-profile', name: '프로필', icon: 'P', path: '/profile' },
+  { id: 'side-intro', name: '인트로', icon: SVG.VELOG, path: '/intro' },
+  { id: 'side-profile', name: '프로필', icon: SVG.VELOG, path: '/profile' },
   {
     id: 'side-project-list',
     name: '프로젝트 목록',
-    icon: 'L',
+    icon: SVG.VELOG,
     path: '/project-list',
   },
   {
     id: 'side-project-portfolio',
     name: '포트폴리오',
-    icon: 'F',
+    icon: SVG.VELOG,
     path: '/portfolio',
   },
   {
     id: 'side-project-onedayhero',
-    name: '원데이히어로',
-    icon: 'O',
+    name: '원데이 히어로',
+    icon: SVG.VELOG,
     path: '/one-day-hero',
   },
   {
     id: 'side-project-musseuk-letter',
-    name: '머쓱레터',
-    icon: 'M',
+    name: '머쓱 레터',
+    icon: SVG.VELOG,
     path: '/musseuk-letter',
   },
-  { id: 'side-project-7elog', name: 'velog 클로닝', icon: 'V', path: '/velog' },
+  {
+    id: 'side-project-7elog',
+    name: 'Velog 클로닝',
+    icon: SVG.VELOG,
+    path: '/velog',
+  },
 ];
 
 export const sidebar = `
