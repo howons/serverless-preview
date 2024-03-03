0fa81df86164c808fe3c208d17be0f76c8776fb3
howons <shinhw371@gmail.com>
2024-03-04 01:15:48 +0900

    feat: 아이콘 추가

--- server/components/portfolio/model.js
+++ server/components/portfolio/model.js
@@ -1,13 +1,40 @@
-import { IMAGE_URL } from '../../utils/images';
+import { ICON_URL, IMAGE_URL } from '../../utils/images';
 import { ROUTE, ROUTE_HASHES } from '../../utils/routes';
 import { project } from '../project/model';
+
+const skills = [
+  { name: 'JavaScript', icon: ICON_URL.JAVASCRIPT },
+  { name: 'Serverless Framework', icon: ICON_URL.SERVERLESS },
+  { name: 'Express', icon: ICON_URL.EXPRESS },
+  { name: 'Webpack', icon: ICON_URL.WEBPACK },
+  { name: 'Sass', icon: ICON_URL.SASS },
+  { name: 'Aws Lambda', icon: ICON_URL.AWS_LAMBDA },
+  { name: 'Amazon S3', icon: ICON_URL.AMAZON_S3 },
+];
 
 const snapshots = [
   {
     id: ROUTE_HASHES[ROUTE.PORTFOLIO][0],
     title: '소개',
-    imageUrl: IMAGE_URL.PORTFOLIO,
-    description: '설명',
+    imageUrl: IMAGE_URL.FAVICON,
+    links: [
+      { name: '깃허브', link: 'https://github.com/howons/serverless-preview' },
+    ],
+    description: `
+      <div class="project__skill-list">
+      ${skills
+        .map(({ name, icon }) => {
+          return `<img src="${icon}" alt="${name}" class="project__skill-icon loading" />`;
+        })
+        .join('')}
+      </div>
+      <br>
+      ${[
+        '개인 포트폴리오 사이트를 만드는 1인 프로젝트입니다.',
+        '프론트엔드는 툴을 사용하지 않고 Vanilla JS로 컴포넌트 시스템을 구현했습니다.',
+        '백엔드는 Express와 Serverless를 사용해 API를 구성했습니다.',
+      ].join('<br>')}
+    `,
   },
   {
     id: ROUTE_HASHES[ROUTE.PORTFOLIO][1],
--- server/components/sidebar/model.js
+++ server/components/sidebar/model.js
@@ -1,30 +1,30 @@
 import { SVG } from '../../utils/images';
 
 export const sidebarItems = [
-  { id: 'side-intro', name: '인트로', icon: SVG.VELOG, path: '/intro' },
-  { id: 'side-profile', name: '프로필', icon: SVG.VELOG, path: '/profile' },
+  { id: 'side-intro', name: '인트로', icon: SVG.INTRO, path: '/intro' },
+  { id: 'side-profile', name: '프로필', icon: SVG.PROFILE, path: '/profile' },
   {
     id: 'side-project-list',
     name: '프로젝트 목록',
-    icon: SVG.VELOG,
+    icon: SVG.LIST,
     path: '/project-list',
   },
   {
     id: 'side-project-portfolio',
     name: '포트폴리오',
-    icon: SVG.VELOG,
+    icon: SVG.PORTFOLIO,
     path: '/portfolio',
   },
   {
     id: 'side-project-onedayhero',
     name: '원데이 히어로',
-    icon: SVG.VELOG,
+    icon: SVG.ONE_DAY_HERO,
     path: '/one-day-hero',
   },
   {
     id: 'side-project-musseuk-letter',
     name: '머쓱 레터',
-    icon: SVG.VELOG,
+    icon: SVG.MUSSEUK,
     path: '/musseuk-letter',
   },
   {
