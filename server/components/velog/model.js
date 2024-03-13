5aa43144a460f9769159149c4d5155df22f77c93
howons <shinhw371@gmail.com>
2024-03-14 02:24:49 +0900

    feat: 벨로그 내용 수정

--- server/components/velog/model.js
+++ server/components/velog/model.js
@@ -32,7 +32,7 @@ const snapshots = [
       <br>
       ${[
         '개발자들을 위한 블로그 서비스인 "Velog"를 클로닝한 프로젝트입니다.',
-        '서울대학교 웹개발 동아리, 와플스튜디오의 입부 팀 프로젝트로 백엔드 4명, 프론트엔드 3명으로 진행했으며 2022년 12월 11일부터 2023년 2월 5일까지 개발했습니다.',
+        '서울대학교 웹개발 동아리인 와플스튜디오의 입부 팀 프로젝트로, 백엔드 4명과 프론트엔드 3명으로 진행했으며 2022년 12월 11일부터 2023년 2월 5일까지 개발했습니다.',
         ' ',
         '프론트엔드 기여도: 50%',
         '주 역할: 텍스트 에디터 개발 및 배포 관리.',
@@ -45,15 +45,15 @@ const snapshots = [
     imageUrl: IMAGE_URL.VELOG_EDIT,
     links: [
       {
-        name: '깃헙 코드',
+        name: '깃헙 코드: 글 작성 페이지',
         link: 'https://github.com/howons/velog-cloning/blob/main/src/pages/Write/index.tsx',
       },
     ],
     description: [
-      'Velog와 동일하게 글 작성이 가능하도록 마크다운 형식의 텍스트 에디터를 구현.',
-      '마크다운 텍스트의 parsing을 위해 remark를 사용.',
-      '마크다운 프리셋 버튼 구현을 위해 텍스트 커서 위치 및 범위 조작 기능이 필요했고, CodeMirror를 활용.',
-      '이미지 업로드 후 비동기 콜백을 이용해 마크다운 이미지 링크에 url 삽입하여 미리보기 기능 구현.',
+      'Velog와 동일하게 글 작성이 가능하도록 <b>마크다운 형식의 텍스트 에디터</b>를 구현했습니다.',
+      '마크다운 텍스트의 parsing을 위해 <b>remark</b>를 사용했습니다.',
+      '에디터 커서 위치에 마크다운 형식을 추가해주는 마크다운 프리셋 버튼 구현을 위해 텍스트 커서 위치 및 범위 조작 기능이 필요했고, <b>CodeMirror</b>를 활용했습니다.',
+      '이미지 업로드 후 비동기 콜백을 이용해 마크다운 이미지 링크에 url을 삽입하여 미리보기 기능을 구현했습니다.',
     ].join('<br>'),
   },
   {
@@ -62,16 +62,16 @@ const snapshots = [
     imageUrl: IMAGE_URL.VELOG_POST,
     links: [
       {
-        name: '깃헙 코드',
+        name: '깃헙 코드: 포스트 페이지',
         link: 'https://github.com/howons/velog-cloning/blob/main/src/pages/PersonalPost/index.tsx',
       },
     ],
     description: [
-      '작성한 포스트 내용을 볼 수 있는 포스트 페이지 구현.',
-      '마찬가지로 마크다운 텍스트의 parsing을 위해 remark를 사용.',
-      '본문에서 heading 요소만 추출해서 TOC 구현.',
-      '좋아요 버튼 및 공유 버튼에 CSS 애니메이션 적용.',
-      '미디어 쿼리를 이용해 반응형 디자인 구현.',
+      '작성한 포스트 내용을 볼 수 있는 포스트 페이지를 구현했습니다.',
+      '에디터와 마찬가지로 마크다운 텍스트의 parsing을 위해 remark를 사용했습니다.',
+      '본문에서 heading 요소만 추출한 뒤, URL에 hash를 추가하고 스크롤 이동할 수 있는 <b>TOC</b>를 구현했습니다.',
+      '좋아요 버튼 및 공유 버튼에 클릭 피드백 및 겹친 버튼 등장을 위한 스타일 애니메이션을 적용했습니다.',
+      '미디어 쿼리를 이용해 페이지의 너비마다 컴포넌트의 표시 여부를 다르게하여 <b>반응형 디자인</b>을 구현했습니다.',
     ].join('<br>'),
   },
   {
@@ -80,14 +80,14 @@ const snapshots = [
     imageUrl: IMAGE_URL.VELOG_DEPLOY,
     links: [
       {
-        name: '깃헙 코드',
+        name: '깃헙 코드: 워크플로우',
         link: 'https://github.com/howons/velog-cloning/blob/main/.github/workflows/main.yml',
       },
     ],
     description: [
-      'React 앱 정적 호스팅 관리.',
-      'Aws S3 및 Cloudfront 사용.',
-      'Github Actions에 workflow를 등록해 main 브랜치로의 PR이 merge될 때 자동배포 등록.',
+      'React 앱의 정적 호스팅을 관리했습니다.',
+      '<b>Aws S3 및 Cloudfront</b> 사용해 배포를 수행했습니다.',
+      'Github Actions에 workflow를 등록하여 main 브랜치로의 PR이 merge될 때 <b>자동 배포</b>가 수행되도록 등록했습니다.',
     ].join('<br>'),
   },
 ];
